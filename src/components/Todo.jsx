f332d3c83b0866b2e665fb2e50b95426e021b2f5
Ebony Gómez <70117322+Bonny0110@users.noreply.github.com>
2021-08-28 23:33:40 -0600

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,12 @@
+  
 import React from 'react'
-
-const Todo = ({todo, index, deleteTodo}) => {
-    return (
-        <>
-            <div className="list">
-            <h3>{todo}</h3> <button className="btn-delete" onClick={() => deleteTodo(index)}>x</button>
-            </div>
-        </>
-    )
+const Todo = ({todoss,numss, indexx, deleteTodo}) => {
+return (
+    <>
+    <div className="list">
+    <h3>{numss} - {todoss}</h3><button className="btn-delete" onClick={()=> deleteTodo(indexx)}>x</button>
+    </div>
+    </>
+)
 }
 export default Todo
