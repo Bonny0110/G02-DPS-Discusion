ad516c94d0c46fadcc199c0a52ef8b27fedc53b6
Ebony Gómez <70117322+Bonny0110@users.noreply.github.com>
2021-08-28 23:32:05 -0600

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,41 +2,59 @@ import React, {useState} from 'react';
 import Todo from '../components/Todo';
 
 const Form = () => {
-    const [todo, setTodo] = useState({})
-    const [todos, setTodos] = useState([
-        {todo: 'Manzanas'},
-        {todo: 'Gaseosas'},
-        {todo: 'Detergente'},
-        {todo: 'Jugo de manzana'}
+    
+    const [num, setTodoo] = useState({
+        
+    })
+    
+    const [todo, setTodo] = useState({
+
+    })
+    const [todos, setTodos] = useState( [
+        {nume: '1', todoo: 'Manzanas'},
+        {nume: '2', todoo: 'Gaseosa'},
+        {nume: '3', todoo: 'Detergente'}
+        
     ])
-
-const handleChange = e => setTodo({[e.target.name]: e.target.value})
-const handleClick = e => {
-    if(Object.keys(todo).length === 0 || todo.todo.trim() === '') {
-        alert('el campo no puede estar vacío')
-        return
+    const handleChange = e => setTodo({[e.target.name]: e.target.value})
+    const handleChang = e => setTodoo({[e.target.name]: e.target.value})
+    const handleClick = e => {
+        if(Object.keys(todo).length === 0 || todo.todo.trim()=== '' || Object.keys(num).length === 0 || num.num.trim()=== ''){
+alert('¡El campo no puede estar vacío!')
+return
+        }
+        setTodos([...todos,{todoo: todo.todo.trim(), nume: num.num.trim()}]);
+       
+       
     }
-    setTodos([...todos, todo])
-}
-
-const deleteTodo = indice => {
-    const newTodos = [...todos]
-    newTodos.splice(indice, 1)
-    setTodos(newTodos)
-}
-
+    
+   const deleteTodo = indice => {
+        const newTodos = [...todos]
+        newTodos.splice(indice, 1)
+        setTodos(newTodos)
+    }
     return (
+        
         <>
         <form onSubmit={e => e.preventDefault()}>
-            <label>Agregar producto</label><br />
-            <input type="text" name="todo" onChange={handleChange}/>
-            <button onClick={handleClick}>AGREGAR</button>
-        </form>
-        {
-            todos.map((value, index) => 
-            (<Todo todo ={value.todo} key={index} index={index} deleteTodo={deleteTodo}/>
+            <label>Agregar tarea</label><br/>
+             <input type="text" name="todo" onChange={handleChange}/>
+             <input type="text" name="num" onChange={handleChang}/>
+            <button onClick={handleClick}>agregar</button>
+            </form>
+            
+       {
+           todos.map((valu, index) => (
+            <Todo todoss={ valu.todoo } numss={valu.nume}  key={index} indexx={index} deleteTodo={deleteTodo} />
+            
             ))
-        }
+           }
+            
+            {
+       }
+          {
+
+       }
         </>
     )
 }
